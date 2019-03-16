c8af5e8fccfeb2362493b7361bda2a841d9f7dca
Marcelo <marceloindart@gmail.com>
2019-03-16 21:00:30 +0000

    event2

--- src/js/views/events.jsx
+++ src/js/views/events.jsx
@@ -1,19 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { Jumbotron, Container } from 'reactstrap';
+import { Context } from "../store/appContext.jsx";
 
-const Example = (props) => {
-  return (
-    <div>
-      <Jumbotron fluid>
-        <Container fluid>
-          <h1 className="display-3">Fluid jumbotron</h1>
-          <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-        </Container>
-      </Jumbotron>
-    </div>
-  );
+import "../../styles/home.css";
+
+import {
+	Card,
+	CardTitle,
+	CardText,
+	CardImg,
+	CardImgOverlay,
+	CardSubtitle
+} from "reactstrap";
+
+const Events = props => {
+	return (
+		<div>
+			<Card inverse>
+				<CardImg
+					width="100%"
+					src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97270&w=318&h=270&bg=333333&txtclr=666666"
+					alt="Card image cap"
+					className="col-3 card-img-top"
+				/>
+				<CardImgOverlay>
+					<CardTitle>Card Title</CardTitle>
+					<CardSubtitle className="pt-50px">
+						This is a wider card with supporting text below as a
+						natural lead-in to additional content. This content is a
+						little bit longer.
+					</CardSubtitle>
+					<CardText>Last updated 3 mins ago</CardText>
+				</CardImgOverlay>
+			</Card>
+		</div>
+	);
 };
 
-export default Example;
+export default Events;
