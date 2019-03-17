0d73316e732e19e9e212ef82ae58390792f52588
Marcelo <marceloindart@gmail.com>
2019-03-17 23:34:04 +0000

    events2

--- src/js/views/events.jsx
+++ src/js/views/events.jsx
@@ -3,36 +3,36 @@ import { Link } from "react-router-dom";
 
 import { Context } from "../store/appContext.jsx";
 
-import "../../styles/home.css";
-
 import {
 	Card,
-	CardTitle,
-	CardText,
 	CardImg,
-	CardImgOverlay,
-	CardSubtitle
+	CardText,
+	CardBody,
+	CardTitle,
+	CardSubtitle,
+	Button
 } from "reactstrap";
 
 const Events = props => {
 	return (
 		<div>
-			<Card inverse>
+			<Card>
 				<CardImg
+					top
 					width="100%"
-					src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97270&w=318&h=270&bg=333333&txtclr=666666"
+					src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
 					alt="Card image cap"
 					className="col-3 card-img-top"
 				/>
-				<CardImgOverlay>
-					<CardTitle>Card Title</CardTitle>
-					<CardSubtitle className="pt-50px">
-						This is a wider card with supporting text below as a
-						natural lead-in to additional content. This content is a
-						little bit longer.
-					</CardSubtitle>
-					<CardText>Last updated 3 mins ago</CardText>
-				</CardImgOverlay>
+				<CardBody>
+					<CardTitle>Card title</CardTitle>
+					<CardSubtitle>Card subtitle</CardSubtitle>
+					<CardText>
+						{
+							"Some quick example text to build on the card title and make up the bulk of the card's content."
+						}
+					</CardText>
+				</CardBody>
 			</Card>
 		</div>
 	);
