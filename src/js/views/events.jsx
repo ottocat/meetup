14aa53afaf1b7bd2096f4fc7f66c810ba8dfefd9
Marcelo <marceloindart@gmail.com>
2019-03-25 22:30:03 +0000

    events3

--- src/js/views/events.jsx
+++ src/js/views/events.jsx
@@ -9,7 +9,7 @@ import { Jumbotron, Container } from "reactstrap";
 const Example = props => {
 	return (
 		<div>
-			<Jumbotron fluid>
+			<Jumbotron className="display bg-info">
 				<Container fluid>
 					<h1 className="display">March 18</h1>
 					<p className="lead">5Th Event for meetup 1</p>
@@ -29,11 +29,15 @@ const Events = props => {
 				color="primary"
 				id="toggler"
 				style={{ marginBottom: "1rem" }}>
-				Log n to RSV
+				Log n to RSVP
 			</Button>
+
 			<UncontrolledCollapse toggler="#toggler">
 				<Card>
 					<CardBody>
+						<a href="page.https#Alink2">
+							https://www.yelp.com/nearme/things-to-do
+						</a>
 						Come in here to meet new and intersting people
 					</CardBody>
 				</Card>
@@ -49,7 +53,7 @@ const Events = props => {
 				<CardBody>
 					<CardTitle>Lets have fun</CardTitle>
 					<CardSubtitle>Get out of your house </CardSubtitle>
-					<CardText>{"Get out of your house ."}</CardText>
+					<CardText>{"Be free and explore the world ."}</CardText>
 				</CardBody>
 			</Card>
 		</div>
