fff3f146b9aba52c853b950248ff4d8e73844a16
Sandra <sandra.menendez003@mymdc.net>
2019-03-28 00:51:41 +0000

    pushing nav

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import PropTypes from "prop-types";
 
 export class Navbar extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			modal: false
+		};
+
+		this.toggle = this.toggle.bind(this);
+	}
+
+	toggle() {
+		this.setState(prevState => ({
+			modal: !prevState.modal
+		}));
+	}
+
 	render() {
 		return (
 			<nav className="navbar navbar-light bg-light mb-3">
@@ -10,14 +27,91 @@ export class Navbar extends React.Component {
 						React Webapp Boilerplate
 					</span>
 				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">
-							Check the Context in action
-						</button>
-					</Link>
-				</div>
+				<Button color="danger" onClick={this.toggle}>
+					Login
+				</Button>
+				<Modal
+					isOpen={this.state.modal}
+					toggle={this.toggle}
+					className={this.props.className}>
+					<ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+					<ModalBody>
+						Lorem ipsum dolor sit amet, consectetur adipisicing
+						elit, sed do eiusmod tempor incididunt ut labore et
+						dolore magna aliqua. Ut enim ad minim veniam, quis
+						nostrud exercitation ullamco laboris nisi ut aliquip ex
+						ea commodo consequat. Duis aute irure dolor in
+						reprehenderit in voluptate velit esse cillum dolore eu
+						fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+						non proident, sunt in culpa qui officia deserunt mollit
+						anim id est laborum.
+					</ModalBody>
+					<ModalFooter>
+						<Button color="primary" onClick={this.toggle}>
+							Do Something
+						</Button>{" "}
+						<Button color="secondary" onClick={this.toggle}>
+							Cancel
+						</Button>
+					</ModalFooter>
+				</Modal>
 			</nav>
 		);
 	}
 }
+
+export default Navbar;
+
+Navbar.propTypes = {
+	// boolean to control the state of the popover
+	isOpen: PropTypes.bool,
+	autoFocus: PropTypes.bool,
+	// if modal should be centered vertically in viewport
+	centered: PropTypes.bool,
+	// corresponds to bootstrap's modal sizes, ie. 'lg' or 'sm'
+	size: PropTypes.string,
+	// callback for toggling isOpen in the controlling component
+	toggle: PropTypes.func,
+	role: PropTypes.string, // defaults to "dialog"
+	// used to reference the ID of the title element in the modal
+	labelledBy: PropTypes.string,
+	keyboard: PropTypes.bool,
+	// control backdrop, see http://v4-alpha.getbootstrap.com/components/modal/#options
+	backdrop: PropTypes.oneOfType([
+		PropTypes.bool,
+		PropTypes.oneOf(["static"])
+	]),
+	// if body of modal should be scrollable when content is long
+	scrollable: PropTypes.bool,
+	// allows for a node/component to exist next to the modal (outside of it). Useful for external close buttons
+	// external: PropTypes.node,
+	// called on componentDidMount
+	onEnter: PropTypes.func,
+	// called on componentWillUnmount
+	onExit: PropTypes.func,
+	// called when done transitioning in
+	onOpened: PropTypes.func,
+	// called when done transitioning out
+	onClosed: PropTypes.func,
+	className: PropTypes.string,
+	wrapClassName: PropTypes.string,
+	modalClassName: PropTypes.string,
+	backdropClassName: PropTypes.string,
+	contentClassName: PropTypes.string,
+	// boolean to control whether the fade transition occurs (default: true)
+	fade: PropTypes.bool,
+	cssModule: PropTypes.object,
+	// zIndex defaults to 1000.
+	zIndex: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+	// backdropTransition - controls backdrop transition
+	// timeout is 150ms by default to match bootstrap
+	// see Fade for more details
+	// backdropTransition: PropTypes.shape(Fade.propTypes),
+	// modalTransition - controls modal transition
+	// timeout is 300ms by default to match bootstrap
+	// see Fade for more details
+	// modalTransition: PropTypes.shape(Fade.propTypes),
+	innerRef: PropTypes.object,
+	// if modal should be destructed/removed from DOM after closing
+	unmountOnClose: PropTypes.bool // defaults to true
+};
